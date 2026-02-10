5b69f4ee966c76a03dc767f2610d5cd3927e8db2
Alex <alexx.rf.18@gmail.com>
2026-02-10 11:56:15 -0300

    adicionados classes de security

---
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,24 @@
+package com.javanauta.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity //isso aqui é pra dizer que é uma tabela do banco de dados //
+@Table(name = "telefone") //especificação do nome da tabela //
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numero", length = 10)
+    private String numero;
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+}
